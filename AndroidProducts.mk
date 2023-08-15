f5633e2ede2595f2f8ada971a6ee6dc1bbbcbdec
Burg1443 <mahanteshmanthu1443@gmail.com>
2023-08-15 18:16:18 +0000

    X2: P404 bring-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2022 The Xperience Project
+# Copyright (C) 2022 The p404 Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xperience_X2.mk
+    $(LOCAL_DIR)/p404_X2.mk
 
 COMMON_LUNCH_CHOICES := \
-    xperience_X2-userdebug \
-    xperience_X2-eng
+    p404_X2-userdebug \
+    p404_X2-eng
--- p404_X2.mk
+++ p404_X2.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The XPerience Project
+# Copyright (C) 2022 The p404 Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,24 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from X2 device
 $(call inherit-product, device/realme/X2/device.mk)
 
-# Inherit some common XPerience stuff
-$(call inherit-product, vendor/xperience/config/common.mk)
+# Inherit some common p404 stuff
+$(call inherit-product, vendor/404/configs/common.mk)
 
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := X2
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := xperience_X2
+PRODUCT_NAME := p404_X2
 PRODUCT_MODEL := realme X2
 BUILD_FINGERPRINT := "realme/RMX1992/RMX1992L1:10/QKQ1.191201.002/1592194586:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 # Rom Specific Flags
-TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_QUICK_TAP  := true
-
-#gapps
-WITH_GMS := true
